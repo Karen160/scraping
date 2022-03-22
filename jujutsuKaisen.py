426312ee8e20de3a427a5d6d76f6edd32549b432
Karen Azoulay <karen.azoulay@edu.devinci.fr>
2022-03-22 15:27:34 +0100

    ADD génére un tableau excel

--- jujutsuKaisen.py
+++ jujutsuKaisen.py
@@ -1,29 +1,31 @@
-from numpy import NaN, character
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import itertools
-# import requests_cache
+import requests_cache
 
-# requests_cache.install_cache('demo_cache')
+# Gère le cache
+requests_cache.install_cache('demo_cache')
 
+# On définit la page listant les informations du personnage en fonction de son nom
 def get_jujutsu_page(character_name):
     page = 'https://jujutsu-kaisen.fandom.com/fr/wiki/' + character_name
     page = requests.get(page).text
     return BeautifulSoup(page, 'html.parser')
 
 
+# On définit la page listant tous les personnages
 def get_character_jujutsu_list_page(url):
     page = requests.get(url).text
     return BeautifulSoup(page, 'html.parser')
 
-
+# On retourne toutes les informations du personnage 
 def get_cara_character(character_name):
     page = get_jujutsu_page(character_name)
     data = {}
 
     for container in page.find_all('aside', {'class': "portable-infobox"}):
-        sections = list(itertools.chain(container.find_all('section', {'class': "pi-group"}, limit=2)))
+        sections = list(itertools.chain(container.find_all('section', {'class': "pi-group"}, limit=3)))
         list_name = list()
         list_info = list()
         for section in sections:
@@ -37,10 +39,10 @@ def get_cara_character(character_name):
     
         for i in range(0, len(list_name)):
             data[list_name[i]] = list_info[i]
-    print(data)
+
     return data
 
-
+# On récupère les noms des personnages en fonction de la limite renseignée
 def get_character(soup, limit):
     characters = soup.find_all(class_='portal', limit= limit)
     name_list = list()
@@ -52,13 +54,21 @@ def get_character(soup, limit):
 
 
 if __name__ == "__main__":
+    # On renseigne le lien contenant la liste des personnages et on récupère le nom de ceux sélectionnée
     path = r'https://jujutsu-kaisen.fandom.com/fr/wiki/Liste_des_Personnages'
-    all_character = get_character(get_character_jujutsu_list_page(path), limit= 15)
+    all_character = get_character(get_character_jujutsu_list_page(path), limit=20)
 
+    # On récupère toutes les informations de nos personnages
     data_to_export = []
-
     for character in all_character:
         data_to_export.append(get_cara_character(character))
 
-    resultats = pd.DataFrame(data_to_export).fillna('indisponible')
+    # On affiche ses informations dans la console sous forme de tableau
+    resultats = pd.DataFrame(data_to_export).dropna(how='all')
+    resultats = resultats.fillna('X')
     print(resultats)
+
+    # On crée un excel listant les caractéristiques des personnages affichés dans la console
+    writer = pd.ExcelWriter('personnages_jujutsu_kaisen.xlsx', engine='xlsxwriter')
+    resultats.to_excel(writer, sheet_name='Sheet1', index=False)
+    writer.save()
